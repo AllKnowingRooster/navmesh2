8c87fa7a4b7d0e5581b72a458196407865117267
AllKnowingRooster <allknowingrooster@gmail.com>
2025-09-10 12:57:01 +0700

    Pull from Git and adding animal Asset

--- Assets/Scenes/tutorial1Scripts/PlayerMovement.cs
+++ Assets/Scenes/tutorial1Scripts/PlayerMovement.cs
@@ -8,8 +8,6 @@ public class PlayerMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private Camera cam;
     private NavMeshAgent agent;
-
-    private RaycastHit[] hitList=new RaycastHit[0];
     void Start()
     {
         agent= GetComponent<NavMeshAgent>();
@@ -18,11 +16,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("Hello");
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {
             Ray cameraToMouse = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-
+            Debug.Log("Hello");
             if (Physics.Raycast(cameraToMouse,out hit))
             {
                 agent.SetDestination(hit.point);
